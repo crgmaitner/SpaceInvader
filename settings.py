cba5b6fa7747ec0ad5c29ccea53ce88941448042
crgmaitner <crgmaitner@users.noreply.github.com>
2021-06-08 16:11:02 -0400

    Starship created and added to screen. Settings file also created.

---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """Class to store all settings for Space Invader."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 700
+        self.bg_color = (230, 230, 230)
