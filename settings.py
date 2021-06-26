55b4f43f65c489bacabda5d441a9cb5f4813567c
crgmaitner <54599177+crgmaitner@users.noreply.github.com>
2021-06-25 21:11:19 -0400

    adjust invader speed.

--- settings.py
+++ settings.py
@@ -14,14 +14,14 @@ class Settings:
         self.ship_limit = 3
 
         # Bullet Settings
-        self.bullet_speed = 2.5
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (255, 255, 255)
         self.bullets_allowed = 4
 
         # Invader Settings
-        self.alien_speed = 1
+        self.alien_speed = 0.5
         self.fleet_drop_speed = 10
         # fleet_direction of 1 represents right, -1 represents left.
         self.fleet_direction = 1
